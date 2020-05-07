98d1fc125e8a2c4719799dc6b059d3aaf2841c45
ArturPiskarev <31780872+ArturPiskarev@users.noreply.github.com>
2020-05-07 19:28:28 +0300

    Add toolы.cs 2

---
+++ NoDeadLine/Tool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+ 
+public class Tools
+{
+	public static int coreCount = 0;
+
+
+    public static string FindToStrEnd(string str, int shift)
+    {
+
+        int t = str.IndexOf('\n');
+        if (t != -1) return str.Substring(0, t + shift);
+
+        else
+            return str;
+    }
+    public static void ClearCurrentConsoleLine()
+    {
+        int currentLineCursor = Console.CursorTop;
+        Console.SetCursorPosition(0, Console.CursorTop);
+        Console.Write(new string(' ', Console.WindowWidth));
+        Console.SetCursorPosition(0, currentLineCursor);
+    }
+
+}
+	
