cbd813257a64ac7ea181fcbbeeaca193f294c70e
ArturPiskarev <31780872+ArturPiskarev@users.noreply.github.com>
2020-05-08 14:00:56 +0300

    Path Saver

--- NoDeadLine/PathSaver/PathSaver/App.xaml.cs
+++ NoDeadLine/PathSaver/PathSaver/App.xaml.cs
@@ -13,5 +13,31 @@ namespace PathSaver
     /// </summary>
     public partial class App : Application
     {
+        public static StartupArgument startupArgument;
+        public  enum StartupArgument
+        {
+            RenderPath,
+            CollectPath
+        }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            
+            if (e.Args.Length == 1)
+            {
+                if (e.Args[0] == "RenderPath")
+                {
+                    startupArgument = StartupArgument.RenderPath;
+                    MessageBox.Show("Choice: \n\n" + e.Args[0]);
+                    MainWindow wnd = new MainWindow();
+                }
+
+                if (e.Args[0] == "CollectPath")
+                {
+                    startupArgument = StartupArgument.CollectPath;
+                    MessageBox.Show("Choice: \n\n" + e.Args[0]);
+                    MainWindow wnd = new MainWindow();
+                }
+            }
+        }
     }
 }
